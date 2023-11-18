b7f71e28112907fae4fb261aaecdce512b489e5d
Hydra <147899615+aH-lk@users.noreply.github.com>
2023-11-18 11:34:59 +0800

    Initial Commit

--- test.py
+++ test.py
@@ -1,31 +1,9 @@
 import os
 from stable_baselines3 import SAC
 import gymnasium as gym
-# folder path
-dir_path = r'/Users/goks/Desktop/MyCode/Python/Adam-v1/saved_models'
-
-# list to store files
-resi = []
-res = []
-models = []
-
-# make env
 env = gym.make('Humanoid-v4', render_mode='human')
 
-# Iterate directory
-for path in os.listdir(dir_path):
-    # check if current path is a file
-    if os.path.isfile(os.path.join(dir_path, path)):
-        resi.append(path)
-
-for i in resi:
-    res.append(i.replace('SAC_', '').replace('.zip', ''))
-
-for fi in range(len(res)):
-    models.append(res[fi])
-
-models.sort(key=int)
-model_path = 'saved_models/' + 'SAC_' + models[-1] + '.zip'
+model_path = 'SAC_1225000.zip'
 model = SAC.load(model_path, env=env)
 obs = env.reset()[0]
 done = False
